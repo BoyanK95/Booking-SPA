2694f993b0ce9c3d509b3331336fb2fa13ab2333
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-11-30 21:27:57 +0200

    Created requester function
    Created requester function for handling requests

---
+++ src/api/api.js
@@ -0,0 +1,45 @@
+const host = 'https://parseapi.back4app.com'
+const appId = 'hl4XKcRdgxJn23q9UbOcECT7TBIM0al7jsLlnggo'
+const apiKey = 'JXSJXaiIw4KCT1VDW7pLJihI6Fl2XPI5fL2bPGXr'
+
+async function request(method, url = '/', data) {
+    const options = {
+        method,
+        headers: {
+            'X-Parse-Application-Id': appId,
+            'X-Parse-JavaScript-Key': apiKey
+        }
+    }
+
+    if (data !== undefined) {
+        options.headers['Content-Type'] = 'application/json'
+        options.body = JSON.stringify(data)
+    }
+
+    //TODO add authorization headers
+
+    try {
+        const responce = await fetch(host + url, options)
+
+        if (responce.status == 204) {
+            return responce   
+        }
+
+        const result = await responce.json()
+
+        if (responce.ok != true) {
+            console.log(result);
+            throw new Error(result.message || result.error)
+        }
+        return result
+
+    } catch (error) {
+        alert(error.message)
+        throw error
+    }
+}
+
+export const get = request.bind(null, 'get')
+export const post = request.bind(null, 'post')
+export const put = request.bind(null, 'put')
+export const del = request.bind(null, 'delete')
--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import { render, html } from './lib/lit-html.js'
-import { until } from './lib/directives/until.js'
+import * as api from './api/api.js'
 
-async function delayed() {
-    await new Promise(r => setTimeout(r, 500))
-
-    return html`<h1>Hello world!</h1>`
-}
-
-
-render(until(delayed(), html`<p>Loading...</p>`), document.body)
+window.api = api
