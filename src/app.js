7a3c6c79bdbd29fa7acc93c695f03d7d66c48f5c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-01 19:16:56 +0200

    Created userNav functionality

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 import page from "./lib/page.mjs";
 import { addRender } from './middlewares/render.js'
 import { addSession } from "./middlewares/session.js";
+import { addUserNav } from "./middlewares/userNav.js";
 import { getUserData } from "./util.js";
 import { catalogView } from "./views/catalog.js";
 import { createView } from './views/create.js'
 import { loginView } from "./views/login.js";
 import { registerView } from "./views/register.js";
+import { navTemplate } from "./views/nav.js";
 
 
-page(addRender(document.querySelector('main')))
+page(addRender(document.querySelector('main'), document.querySelector('header')))
 page(addSession(getUserData))
+page(addUserNav(navTemplate))
 page("/", '/create')
 page('/rooms', catalogView)
 page('/rooms/:id', ({ params: {id} })=> console.log('details', id))
