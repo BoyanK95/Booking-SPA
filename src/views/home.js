675d109dcb9ca3926760236068d003bde1b16268
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-07 15:22:45 +0200

    Updated css

--- src/views/home.js
+++ src/views/home.js
@@ -6,8 +6,8 @@ const homeTemplate = (user) => html`
     ${user ? html`<p id="welcome-user-msg">Hello, ${user.username} !</p>` 
     : ''}
     <h1>Welcome to my Booking page</h1>
-    <img id="homeImg" src="../../images/sunrise-city.webp" alt="">
     <h3>If you are looking for a room to rent ...<a href="/rooms"> Click here !</a></h3>
+    <img id="homeImg" src="../../images/sunrise-city.webp" alt="">
     ${user ? html`
     <h3>Do you want to post your room for rent ...<a href="/create"> Click here !</a></h3>` : ''}
     
