a27d0f182df561c115d07d6eea45a325e4785e1c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-12-14 16:07:46 +0200

    Updated the style
    Created more cyber-punk feel style for the site

--- src/views/home.js
+++ src/views/home.js
@@ -1,20 +1,25 @@
 import { html } from "../lib/lit-html.js";
 
-
-const homeTemplate = (user) => html`
-<div class="home">
-    ${user ? html`<p id="welcome-user-msg">Hello, ${user.username} !</p>` 
-    : ''}
-    <h1>Welcome to my Booking page</h1>
-    <h3>If you are looking for a room to rent ...<a href="/rooms"> Click here !</a></h3>
-    <img id="homeImg" src="../../images/sunrise-city.webp" alt="">
-    ${user ? html`
-    <h3>Do you want to post your room for rent ...<a href="/create"> Click here !</a></h3>` : ''}
-    
-</div>`
+const homeTemplate = (user) => html` 
+<header class="home">
+  ${user ? html`<p id="welcome-user-msg">Hello, ${user.username} !</p>` : ""}
+  <div id="homeImg">
+    <img src="../../images/digitalCity.jpg" alt="" />
+  </div>
+  <h1>Welcome to my Booking page</h1>
+  <h3>
+    If you are looking for a room to rent ...<a href="/rooms"> Click here !</a>
+  </h3>
+  ${user
+    ? html` <h3>
+        Do you want to post your room for rent ...<a href="/create">
+          Click here !</a
+        >
+      </h3>`
+    : ""}
+</header>`;
 
 export function homeView(ctx) {
-    const user = ctx.user
-    ctx.render(homeTemplate(user))
-
+  const user = ctx.user;
+  ctx.render(homeTemplate(user));
 }
